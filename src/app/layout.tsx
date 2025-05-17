36b57075fc7e7166969f0cf1a1618cea35dccb15
rogeriobragapt <rogeriosilva.cunha1@gmail.com>
2025-05-17 19:21:22 +0100

    Adicionar layout raiz

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Blaze Crash Analyzer',
+  description: 'Analisador de padrões para o jogo Crash',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
